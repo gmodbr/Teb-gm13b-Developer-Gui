45c88377aa283f18a286c344698552c17bbbe7ea
TheEndBoss_101 <94846253+TheEndBoss-101@users.noreply.github.com>
2022-03-22 16:09:31 -0500

    Update and rename arc_init.lua to teb_init.lua

---
+++ lua/autorun/teb_init.lua
@@ -0,0 +1,15 @@
+--[[
+	Forked from:
+	https://github.com/Xalalau/Anomaly-Research-Center-ARC
+
+	Created by Xalalau(Og Addon) and the GMod Dev community, 2022 :)
+	MIT License
+]]
+
+CGM13D = { -- Community GM13 Developer
+	Gui = {}
+}
+
+hook.Add("Initialize", "teb_int", function()
+	GM13:AddBase("teb")
+end)
