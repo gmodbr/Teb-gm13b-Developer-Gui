58c692e0d6268d914d1cb3e93e798378ce0e2cd0
TheEndBoss_101 <94846253+TheEndBoss-101@users.noreply.github.com>
2022-03-24 22:18:43 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -4,9 +4,29 @@ A simple developer gui.
 
 # How to set up the addon
 
-The only thing you need to do is clone the repository inside GMod addons folder and keep the directory name.
+The only thing you need to do is clone the repository inside GMod addons folder and keep the directory names.
 
-MIT License
+# Images
+
+![Main Tab](/img/main.png "Main Tab")  
+![Memories Tab](/img/mem.png "Memories Tab")  
+![Map Tab](/img/maptop.png "Map Tab")  
+![Map Tab](/img/mapbtm.png "Map Tab")
+
+# Q & A / FAQ
+
+> Q: How do i download?  
+> A: Click code and download as a .zip
+
+> Q: What addon folder?  
+> A: C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\addons
+
+> Q: What do you mean by unzip?  
+> A: What?
+
+> Q: Where can I make a bug report?  
+> A: [Here](https://github.com/TheEndBoss-101/Teb-gm13b-Developer-Gui/issues)
+
 # Links
 
 [gm_construct_13 beta](https://steamcommunity.com/sharedfiles/filedetails/?id=2553727051)
@@ -16,3 +36,7 @@ MIT License
 [ARC Discord](https://discord.gg/97UpY3D7XB)
 
 [AMTF Discord](https://discord.gg/dw3rVqMhr7)
+
+# License
+
+MIT License
--- lua/teb/base/sh_gui_g.lua
+++ lua/teb/base/sh_gui_g.lua
@@ -1,5 +1,5 @@
 -- I have no idea what i am doing. - TheEndBoss_101
--- 3/23/2022 320 lines, This is getting big
+-- 3/24/2022 276 lines, This is big
 
 local cgm13bd_vugi_debug = true 
 
@@ -17,7 +17,6 @@ if CLIENT then
       DermaFrame:SetDeleteOnClose(true)
       DermaFrame:SetMouseInputEnabled(true)
       DermaFrame:SetKeyboardInputEnabled(true)
-      --DermaFrame:SetTextColor(Color(50, 53, 59))
       DermaFrame:MakePopup()
       DermaFrame:Center()
 
@@ -28,24 +27,19 @@ if CLIENT then
       PropertySheet:Dock(FILL)
 
       local MainTab = vgui.Create("DPanel")
-      --MainTab:SetParent( PropertySheet )
       MainTab:SetBackgroundColor(Color(51, 51, 51))
 
       local MemoriesTab = vgui.Create("DPanel")
-      --MemoriesTab:SetParent( PropertySheet )
       MemoriesTab:SetBackgroundColor(Color(51, 51, 51))
 
       local MapTab = vgui.Create("DPanel")
-      --MapTab:SetParent( PropertySheet )
       MapTab:SetBackgroundColor(Color(51, 51, 51))
       local MapTab_PropertySheet = vgui.Create("DPropertySheet")
       MapTab_PropertySheet:SetParent(MapTab)
       MapTab_PropertySheet:Dock(FILL)
       local MapTab_Top = vgui.Create("DPanel")
-      --MapTab:SetParent( PropertySheet )
       MapTab:SetBackgroundColor(Color(51, 51, 51))
       local MapTab_Btm = vgui.Create("DPanel")
-      --MapTab:SetParent( PropertySheet )
       MapTab:SetBackgroundColor(Color(51, 51, 51))
       -- Mini Map Tab
       -- top
@@ -60,23 +54,20 @@ if CLIENT then
       MapTab_Image_btm:SetSize(396, 396)
       MapTab_Image_btm:SetImage( "teb/cgm13d/bottom" )
       --MapTab_Image_btm:SizeToContents()
-
+      local wtrmrk = vgui.Create("DImage", DermaFrame) --mobenixcitizen2007 sugested this. TODO:Add a dot ploting a plr pos.
+      wtrmrk:SetPos(570, 440)
+      wtrmrk:SetSize(60, 85)
+      --MapTab_Image_btm:SizeToContents()
+      wtrmrk:SetImage( "teb/cgm13d/cone" )
       
       -- Main Tab
       -- Desc
       local Main_Tab_List = vgui.Create("DScrollPanel", MainTab)
-      --Main_Tab_List:SetSpacing(5)
-      --Main_Tab_List:EnableHorizontal(false)
-      --Main_Tab_List:EnableVerticalScrollbar(true)
-      --MainTab:SetContents(Main_Tab_List)
       Main_Tab_List:Dock(FILL)
-      --Main_Tab_List:SetAutoSize(true)
-      --Main_Tab_List:SizeToContents()
-      --Main_Tab_List:AddItem()	
 
       local Main_Tab_Label = vgui.Create("DLabel")
       Main_Tab_Label:SetPos(5,5)
-      Main_Tab_Label:SetText("The main Property Sheet")
+      Main_Tab_Label:SetText(" The main Property Sheet")
       Main_Tab_Label:SizeToContents()
       Main_Tab_Label:Dock( TOP )
 	   Main_Tab_Label:DockMargin( 0, 0, 0, 5 )
@@ -85,7 +76,6 @@ if CLIENT then
       -- Toggle DevMode
       local Devmode = vgui.Create("DCheckBoxLabel")
       Devmode:SetText("Developer Mode")
-      --Devmode:SetPos(5,25)
       Devmode:SizeToContents()
       Devmode:Dock( TOP )
 	   Devmode:DockMargin( 0, 0, 0, 5 )
@@ -95,15 +85,14 @@ if CLIENT then
          Devmode:SetValue(false)
       end
       function Devmode:OnChange(val)
+         GM13:ToggleDevMode()
          GM13.devMode = val
          if cgm13bd_vugi_debug then print("GM13.devMode",val) end
       end
-      Main_Tab_List:AddItem(Devmode)	
+      Main_Tab_List:AddItem(Devmode)
 
       -- Tier Slider
       local TierSlider = vgui.Create("DNumSlider")
-      --TierSlider:SetPos(5,40)
-      --TierSlider:SetSize(150, 45)
       TierSlider:SetText("Edit Tier")
       TierSlider:SetMin(1)
       TierSlider:SetMax(4)
@@ -117,8 +106,6 @@ if CLIENT then
 
       -- Reset Button
       local ResetButton = vgui.Create("DButton")
-      --ResetButton:SetPos(5,75)
-      --ResetButton:SetSize(100, 20)
       ResetButton:SetText("Reset The Addon")
       ResetButton:SetConsoleCommand("gm13_reset", "yes")
       ResetButton:Dock( TOP )
@@ -129,18 +116,10 @@ if CLIENT then
       -- Memories Tab
       -- Desc
       local Memories_Tab_List = vgui.Create("DScrollPanel", MemoriesTab)
-      --Memories_Tab_List:SetSpacing(5)
-      --Memories_Tab_List:EnableHorizontal(false)
-      --Memories_Tab_List:EnableVerticalScrollbar(true)
-      --MainTab:SetContents(Main_Tab_List)
       Memories_Tab_List:Dock(FILL)
-      --Memories_Tab_List:SetAutoSize(true)
-      --Memories_Tab_List:SizeToContents()
-      --Memories_Tab_List:Item()	
 
       local Memories_Tab_Label = vgui.Create("DLabel")
-      --Memories_Tab_Label:SetPos(5,5)
-      Memories_Tab_Label:SetText("Edit Memories")
+      Memories_Tab_Label:SetText(" Add Memories")
       Memories_Tab_Label:Dock( TOP )
 	   Memories_Tab_Label:DockMargin( 0, 0, 0, 5 )
       Memories_Tab_Label:SizeToContents()
@@ -148,8 +127,6 @@ if CLIENT then
 
       -- savedCitizen
       local Memory_savedCitizen_Button = vgui.Create("DButton")
-      --Memory_savedCitizen_Button:SetPos(5,30)
-      --Memory_savedCitizen_Button:SetSize(100, 15)
       Memory_savedCitizen_Button:SetText("Get The Cone (savedCitizen)")
       Memory_savedCitizen_Button:Dock( TOP )
 	   Memory_savedCitizen_Button:DockMargin( 0, 0, 0, 5 )
@@ -164,8 +141,6 @@ if CLIENT then
 
       -- ratmanInit
       local Memory_ratmanInit_Button = vgui.Create("DButton")
-      --Memory_ratmanInit_Button:SetPos(5,60)
-      --Memory_ratmanInit_Button:SetSize(100, 15)
       Memory_ratmanInit_Button:SetText("Spawn Ratman (ratmanInit)")
       Memory_ratmanInit_Button:Dock( TOP )
 	   Memory_ratmanInit_Button:DockMargin( 0, 0, 0, 5 )
@@ -180,8 +155,6 @@ if CLIENT then
 
       --BigDarkRoom
       local Memory_BigDarkRoom_Button = vgui.Create("DButton")
-      --Memory_BigDarkRoom_Button:SetPos(5,90)
-      --Memory_BigDarkRoom_Button:SetSize(100, 15)
       Memory_BigDarkRoom_Button:SetText("Big Dark Room (???)")
       Memory_BigDarkRoom_Button:Dock( TOP )
 	   Memory_BigDarkRoom_Button:DockMargin( 0, 0, 0, 5 )
@@ -197,8 +170,6 @@ if CLIENT then
       -- Transmissions
       local TransmissionsCategory = vgui.Create("DCollapsibleCategory")
       TransmissionsCategory:SetLabel("Transmissions")
-      --TransmissionsCategory:SetPos(5, 120)
-      --TransmissionsCategory:SetSize(250, 100)
       TransmissionsCategory:SetExpanded(false)
       TransmissionsCategory:Dock( TOP )
 	   TransmissionsCategory:DockMargin( 0, 0, 0, 5 )
@@ -206,20 +177,14 @@ if CLIENT then
       Memories_Tab_List:AddItem(TransmissionsCategory)
 
       local TransCategoryList = vgui.Create("DScrollPanel", TransmissionsCategory)
-      --TransCategoryList:SetSpacing(5)
-      --TransCategoryList:EnableHorizontal(false)
-      --TransCategoryList:EnableVerticalScrollbar(true)
       TransCategoryList:Dock(FILL)
       TransCategoryList:AddItem(TransCategoryList)
-      --transmissionsCategory:SetContents(TransCategoryList)
 
       --transmission1
-      local Memory_transmission1_Button = vgui.Create("DButton")
-      --Memory_transmission1_Button:SetPos(5,90)
-      --Memory_transmission1_Button:SetSize(100, 15)
+      local Memory_transmission1_Button = vgui.Create("DButton", TransCategoryList)
       Memory_transmission1_Button:SetText("Transmission 1 (transmission1)")
-      --Memory_transmission1_Button:Dock( TOP )
-	   --Memory_transmission1_Button:DockMargin( 0, 0, 0, 5 )
+      Memory_transmission1_Button:Dock( TOP )
+	   Memory_transmission1_Button:DockMargin( 0, 0, 0, 5 )
       Memory_transmission1_Button:SizeToContents()
       Memory_transmission1_Button.DoClick = function()
          net.Start("Memory")
@@ -231,8 +196,6 @@ if CLIENT then
 
       --transmission2
       local Memory_transmission2_Button = vgui.Create("DButton")
-      --Memory_transmission2_Button:SetPos(5,90)
-      --Memory_transmission2_Button:SetSize(100, 15)
       Memory_transmission2_Button:SetText("Transmission 2 (transmission2)")
       Memory_transmission2_Button:Dock( TOP )
 	   Memory_transmission2_Button:DockMargin( 0, 0, 0, 5 )
@@ -247,8 +210,6 @@ if CLIENT then
       
       --transmission3
       local Memory_transmission3_Button = vgui.Create("DButton")
-      --Memory_transmission3_Button:SetPos(5,90)
-      --Memory_transmission3_Button:SetSize(100, 15)
       Memory_transmission3_Button:SetText("Transmission 3 (transmission3)")
       Memory_transmission3_Button:Dock( TOP )
 	   Memory_transmission3_Button:DockMargin( 0, 0, 0, 5 )
@@ -263,8 +224,6 @@ if CLIENT then
 
        --transmissionall
       local Memory_transmissionall_Button = vgui.Create("DButton")
-      --Memory_transmissionall_Button:SetPos(5,90)
-      --Memory_transmissionall_Button:SetSize(100, 15)
       Memory_transmissionall_Button:Dock( TOP )
 	   Memory_transmissionall_Button:DockMargin( 0, 0, 0, 5 )
       Memory_transmissionall_Button:SetText("All Transmissions")
@@ -292,16 +251,13 @@ if CLIENT then
 end
 
 if SERVER then
-   concommand.Add("cgm13d_devmode",enabledevsrv)
-   function enabledevsrv()
-      if GM13.devMode then
-         GM13.devMode = 0
-         if cgm13bd_vugi_debug then print("Server Dev Mode Off") end
-      else
-         GM13.devMode = 1
-         if cgm13bd_vugi_debug then print("Server Dev Mode On") end
-      end
+   util.AddNetworkString("toggledevsrv_net")
+   local toggledevsrv_func = function() --IF ERROR INGORE 3/24/2022
+      GM13:ToggleDevMode() --Xalalau, Thank you. You don't know how mutch that means.
+      if cgm13bd_vugi_debug then print("GM13 Dev Mode " .. (GM13.devMode and "On" or "Off")) end
    end
+   net.Receive("toggledevsrv_net", toggledevsrv_func)
+   concommand.Add("cgm13d_devmode", toggledevsrv_func)
 
    util.AddNetworkString("Memory")
    local MsgNameReceived = function()
