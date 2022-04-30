f6310b3110e4fbc18fa5d5f02242d78e3a8d8397
Xalalau Xubilozo <xalalau@outlook.com>
2022-04-30 16:43:19 -0300

    Update initialization file

--- lua/autorun/theendboss101_init.lua
+++ lua/autorun/theendboss101_init.lua
@@ -6,17 +6,15 @@
 	MIT License
 ]]
 
-C_GM13B_GUI = { -- Community GM13 Developer
---CGM13 = { -- Community GM13
-	name = "C_GM13B_GUI",
-	Gui = {},
-	--DevMode = false,
-	toolCategories = {"C_GM13B_GUI_DEV"} -- Devmove tool categories
+C_GM13B_GUI = { -- Community GM13 Developer GUI
+	luaFolder = "theendboss101/cgm13bgui",
+	toolCategories = { -- Devmove tool categories
+		"C_GM13B_GUI",
+		"C_GM13B_GUI_DEV"
+	},
+	Gui = {}
 }
 
---hook.Add("Initialize", CGM13.name .. "_int", function()
-hook.Add("Initialize", C_GM13B_GUI.name .. "_int", function()
-	--GM13:AddBase("theendboss101/cgm13bgui") -- Legacy
-	GM13:IncludeBase("theendboss101/cgm13bgui")
-	--GM13:IncludeBase(C_GM13B_GUI)
+hook.Add("Initialize", C_GM13B_GUI.luaFolder .. "_int", function()
+	GM13:IncludeBase(C_GM13B_GUI)
 end)
